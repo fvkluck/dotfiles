ae95a72d99659e144b3b422b585dd956c81b6496
FlorianGit <f.v.kluck@gmail.com>
2024-07-13 21:15:05 +0200

    Upgrade packages and recompile fzf.fnl

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,2 +1,7 @@
 -- [nfnl] Compiled from fnl/plugins/fzf.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"junegunn/fzf"}, {"junegunn/fzf.vim"}}
+local function _1_()
+  vim.keymap.set("n", "<c-p>", ":FZF<CR>", {noremap = true})
+  vim.keymap.set("n", "\\", ":Ag<CR>")
+  return vim.keymap.set("n", "<leader>fw", ":grep! \8<C-R><C-W>\8<CR>:cw<CR>")
+end
+return {{"junegunn/fzf"}, {"junegunn/fzf.vim", init = _1_}}
