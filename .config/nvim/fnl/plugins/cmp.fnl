d54057b796e8cb6b75bd584efb2d97299011b815
fvkluck <f.v.kluck@gmail.com>
2024-08-25 17:53:33 +0200

    Latest changes to dotfiles

--- .config/nvim/fnl/plugins/cmp.fnl
+++ .config/nvim/fnl/plugins/cmp.fnl
@@ -30,6 +30,8 @@
               (cmp.setup {:formatting {:format (fn [entry item]
                                                  (set item.menu (or (. cmp-src-menu-items entry.source.name) ""))
                                                  item)}
+                          :preselect "none"
+                          :completion {:completeopt "menu,menuone,noinsert,noselect"}
                           :mapping {:<C-p> (cmp.mapping.select_prev_item)
                                     :<C-n> (cmp.mapping.select_next_item)
                                     :<C-b> (cmp.mapping.scroll_docs (- 4))
@@ -37,7 +39,7 @@
                                     :<C-Space> (cmp.mapping.complete)
                                     :<C-e> (cmp.mapping.close)
                                     :<CR> (cmp.mapping.confirm {:behavior cmp.ConfirmBehavior.Insert
-                                                                :select true})
+                                                                :select false})
                                     :<Tab> (cmp.mapping (fn [fallback]
                                                           (if
                                                             (cmp.visible) (cmp.select_next_item)
--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -7,15 +7,15 @@ vim.keymap.set("n", "gh", ":w<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>q", ":q<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>ev", ":e $MYVIMRC<CR>", {noremap = true})
 vim.keymap.set("n", "<leader>wd", ":cd %:h<CR>", {noremap = true})
-vim.keymap.set("n", "<F3>", ":cprev<CR>", {noremap = true})
-vim.keymap.set("n", "<F4>", ":cnext<CR>", {noremap = true})
+vim.keymap.set("n", "<C-N>", ":cprev<CR>", {noremap = true})
+vim.keymap.set("n", "<C-M>", ":cnext<CR>", {noremap = true})
 vim.keymap.set("n", "<localleader>/", ":nohlsearch<CR>", {noremap = true})
-vim.keymap.set("n", "<c-p>", ":FZF<CR>", {noremap = true})
 do
-  local options = {expandtab = true, list = true, listchars = "tab:>-,trail:.,extends:>", clipboard = "unnamedplus", tabstop = 2, shiftwidth = 2, softtabstop = 2, completeopt = "menuone,noselect", ignorecase = true, undofile = true, undodir = "~/.vim/undo", undolevels = 1000, undoreload = 10000, smartcase = true, number = true, relativenumber = true, ruler = true, signcolumn = "number", grepprg = "ag --vimgrep --nogroup --nocolor -s --ignore tags", termguicolors = true}
+  local options = {expandtab = true, list = true, listchars = "tab:>-,trail:.,extends:>", clipboard = "unnamedplus", tabstop = 2, shiftwidth = 2, softtabstop = 2, completeopt = "menuone,noselect", undofile = true, undodir = "/Users/florian/.vim/undo", undolevels = 1000, undoreload = 10000, number = true, relativenumber = true, ruler = true, signcolumn = "number", grepprg = "ag --vimgrep --nogroup --nocolor -s --ignore tags", termguicolors = true}
   for option, value in pairs(options) do
     core.assoc(vim.o, option, value)
   end
 end
+core.assoc(vim.g, "netrw-keepdir", 0)
 vim.cmd.colorscheme("neosolarized")
 return {}
