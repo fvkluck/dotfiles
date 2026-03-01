38d76e3121a1856ce535c57a2f5bf1ced31bcc7d
fvkluck <f.v.kluck@gmail.com>
2026-03-01 08:02:47 +0100

    Add some git keybindings

--- .config/nvim/fnl/plugins/fugitive.fnl
+++ .config/nvim/fnl/plugins/fugitive.fnl
@@ -1,2 +1,9 @@
+(vim.keymap.set :n :gl ":vertical :G log --decorate<CR>" {:noremap true})
+(vim.keymap.set :n :<leader>g ":vertical :G<CR>" {:noremap true})
+(vim.keymap.set :n :<leader>gl ":vertical :G log<CR>" {:noremap true})
+(vim.keymap.set :n :<leader>gb ":G blame<CR>" {:noremap true})
+(vim.keymap.set :n :<leader>gc ":vertical :G commit<CR>" {:noremap true})
+
 [{1 :tpope/vim-fugitive}
 {1 :rhysd/committia.vim}]
+
--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,2 +1,7 @@
--- [nfnl] Compiled from fnl/plugins/fugitive.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] fnl/plugins/fugitive.fnl
+vim.keymap.set("n", "gl", ":vertical :G log --decorate<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>g", ":vertical :G<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>gl", ":vertical :G log<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>gb", ":G blame<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>gc", ":vertical :G commit<CR>", {noremap = true})
 return {{"tpope/vim-fugitive"}, {"rhysd/committia.vim"}}
